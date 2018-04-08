3a636e30ccac9db67bcca91e26f04ce9432a91ef
kaosborn <kaosborn@users.noreply.github.com>
2018-04-07 19:32:09 -0700

    bump year to 2018

--- Combinatorics/Multicombination.cs
+++ Combinatorics/Multicombination.cs
@@ -2,7 +2,7 @@
 // Project: KaosCombinatorics
 // File: Multicombination.cs
 //
-// Copyright © 2009-2017 Kasey Osborn (github.com/kaosborn)
+// Copyright © 2009-2018 Kasey Osborn (github.com/kaosborn)
 // MIT License - Use and redistribute freely
 //
 
