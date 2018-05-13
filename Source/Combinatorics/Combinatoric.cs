d5f0d65334b2724e8dd519bd942aee5d53ccacb0
kaosborn <kaosborn@users.noreply.github.com>
2018-05-12 19:32:34 -0700

    mark as CLSCompliant

--- Source/Combinatorics/Combinatoric.cs
+++ Source/Combinatorics/Combinatoric.cs
@@ -9,6 +9,7 @@
 using System;
 using System.Collections.Generic;
 
+[assembly: CLSCompliant (true)]
 namespace Kaos.Combinatorics
 {
     /// <summary>Provides static methods for combinatorics.</summary>
@@ -19,7 +20,7 @@ namespace Kaos.Combinatorics
 
         static private readonly long[] factorial = new long[]
         {
-            1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800, 39916800, 
+            1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800, 39916800,
             479001600, 6227020800, 87178291200, 1307674368000, 20922789888000,
             355687428096000, 6402373705728000, 121645100408832000, 2432902008176640000
         };
