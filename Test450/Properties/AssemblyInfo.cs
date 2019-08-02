3cc75cfeca427717429a29ad9e7b6e0e22b83f91
kaosborn <kaosborn@users.noreply.github.com>
2019-08-01 23:27:22 -0700

    add Test450 project

---
+++ Test450/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle ("Test450")]
+[assembly: AssemblyDescription ("")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("")]
+[assembly: AssemblyProduct ("KaosCombinatorics")]
+[assembly: AssemblyCopyright ("")]
+[assembly: AssemblyTrademark ("freeware")]
+[assembly: AssemblyCulture ("")]
+[assembly: ComVisible (false)]
+[assembly: Guid ("37950e4f-f817-43d4-b537-e87746fbc2b5")]
+[assembly: AssemblyVersion ("0.1.0.0")]
